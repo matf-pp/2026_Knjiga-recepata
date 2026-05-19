6286006edd4065bdda7e6c7c4ed1264dcf5d814e
AndrijaRadovicDB <ane.005.ar@gmail.com>
2026-05-19 14:19:48 +0200

    implementiran skup

---
+++ models/set.go
@@ -0,0 +1,38 @@
+package models
+
+type Set struct {
+	// Vrednosti su prazne strukture i one ne zauzimaju memoriju
+	// Koristimo pokazivace jer mapa mora da podrzava ==
+	// i da ne bi kopirali celu strukturu
+	elements map[*Recipe]struct{}
+}
+
+// Pravimo novi skup
+func NewSet() *Set {
+	return &Set{
+		elements: make(map[*Recipe]struct{}),
+	}
+}
+
+// Dodajemo element u skup
+func (s *Set) Add(value *Recipe) {
+	s.elements[value] = struct{}{}
+}
+
+// Uklanjamo element iz skupa
+func (s *Set) Remove(value *Recipe) {
+	delete(s.elements, value)
+}
+
+// Proverava jel element u skupu
+func (s *Set) Contains(value *Recipe) bool {
+	// Pristupanje elementu mape nam vraca taj element ako postoji i vrednost true
+	// inace nula vrednost za taj tip i false
+	_, found := s.elements[value]
+	return found
+}
+
+// Vraca velicinu skupa
+func (s *Set) Size() int {
+	return len(s.elements)
+}
