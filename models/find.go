b3fefeff7beff65c0a390825d4dbd2462cdfb37f
AndrijaRadovicDB <ane.005.ar@gmail.com>
2026-05-19 15:33:34 +0200

    implementirana efikasnija pretraga

--- models/find.go
+++ models/find.go
@@ -1,41 +1,73 @@
 package models
 
-func ArrayToSet(recipes []*Recipe) *Set {
-	set := NewSet()
-
-	for _, r := range recipes {
-		set.Add(r)
-	}
-
-	return set
+// Struktura podataka za brzu pretragu, umesto da za svaki recept
+// gledamo namirnice, mi za svaku namernicu znamo koji je svi recepti koriste
+type InvertedIndex struct {
+	// Kljuc je ime namirnice,
+	// a vrednost je skup svih recepata koji je koriste
+	index map[string]*Set
 }
 
-func (set *Set) FilterByIngredient(ingredient *Ingredient) {
-	ingredientName := ingredient.Name
+func NewInvertedIndex() *InvertedIndex {
+	return &InvertedIndex{
+		index: make(map[string]*Set),
+	}
+}
 
-	for r := range set.elements {
-		foundIngredient := false
+// Pravimo index, ovo radimo na pocetku samo 1
+func (ii *InvertedIndex) AddRecipe(r *Recipe) {
+	for _, ingredient := range r.Ingredients {
+		ingredientName := ingredient.Name
 
-		for _, i := range r.Ingredients {
-			if i.Name == ingredientName {
-				foundIngredient = true
-				break
+		// Ako smo pronasli novu namernicu dodajemo je u mapu
+		if ii.index[ingredientName] == nil {
+			ii.index[ingredientName] = NewSet()
+		}
+
+		// Dodajemo recept u skup za datu namirnicu
+		ii.index[ingredientName].Add(r)
+	}
+}
+
+// Filtriramo vec postojece inverted index
+func (ii *InvertedIndex) Filter(ingredients []*Ingredient) *Set {
+	ingredientsLen := len(ingredients)
+
+	if ingredientsLen == 0 {
+		return NewSet()
+	}
+
+	ingredientName := ingredients[0].Name
+	first := ii.index[ingredientName]
+	// Ako nema prve namirnice nema smisla dalje traziti
+	if first == nil {
+		return NewSet()
+	}
+
+	result := NewSet()
+	for r := range first.elements {
+		result.Add(r)
+	}
+
+	// Vrsimo presek skupova
+	for i := 1; i < ingredientsLen; i++ {
+		ingredientName = ingredients[i].Name
+		next := ii.index[ingredientName]
+		// Ako nema ni jedan recept namernicu to je samo prazan skup
+		if next == nil {
+			return NewSet()
+		}
+
+		tmp := NewSet()
+
+		for r := range result.elements {
+			if next.Contains(r) {
+				tmp.Add(r)
 			}
 		}
 
-		if !foundIngredient {
-			set.Remove(r)
-		}
+		result = tmp
 	}
 
-}
-
-func FilterByIngredients(ingredients []*Ingredient, recipes []*Recipe) *Set {
-	set := ArrayToSet(recipes)
-
-	for _, i := range ingredients {
-		set.FilterByIngredient(i)
-	}
-
-	return set
+	return result
 }
